197dea05c2f09a4528584e759812c6937a40880a
eloyms262 <eloyMs26@gmail.com>
2020-12-15 09:39:28 +0800

    set up server

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const c = require('config');
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
+
+connectDB();
+
 app.get('/', (req, res) => res.send('API Running'));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
